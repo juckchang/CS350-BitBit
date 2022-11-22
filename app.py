fb433c3a879ffadc688feea5f997e55d9ae35f83
juckchang <c2w2m2@kaist.ac.kr>
2022-11-22 21:45:22 +0900

    [Add] summaizer

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, flash, json, jsonify
+from flask import Flask, request, flash, json, jsonify, send_file
+from summarizer import Summarizer
 from uuid import uuid4
+import string
 import os
 import fitz
 
@@ -56,7 +58,47 @@ def summarize():
   except Exception as e:
     print(e)
   
-  return ''
+  count = 0
+  count += sum([text.count(x) for x in string.printable])
+  if count / len(text) < 0.95:
+    return jsonify(status='fail', message='English text must be 95%')
+  
+  summarize = model(text, num_sentences=3)
+  print(summarize)
+
+  filename = uuid4()
+
+  doc = fitz.open()
+  page = doc.new_page()
+  fill_rect = fitz.Rect(0, 0, 1920, 1080)
+  tw = fitz.TextWriter(page.rect)
+  tw.fill_textbox(fill_rect, summarize, align=fitz.TEXT_ALIGN_LEFT)
+  tw.write_text(page)
+  doc.save(f'result/{filename}.pdf')
+
+  return jsonify(status='ok', message=f'{filename}.pdf')
+
+@app.route('/api/download', methods=['POST'])
+def download():
+  if not request.is_json:
+    return jsonify(status='fail', message='Not json request')
+  data = request.get_json()
+
+  if 'file' not in data:
+    return jsonify(status='fail', message='File list not provided')
+
+  file = data['file']
+
+  file = file.replace('..', '').replace('/', '')
+
+  if file.split('.').pop() != 'pdf':
+    return jsonify(status='fail', message='File is not pdf')
+
+  if not os.path.exists(f'result/{file}'):
+    return jsonify(status='fail', message='File not exists')
+
+  return send_file(f'result/{file}', mimetype='application/pdf', attachment_filename='summarize.pdf', as_attachment=True)
 
 if __name__ == '__main__':
+  model = Summarizer()
   app.run()
